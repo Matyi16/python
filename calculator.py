62ae6394319416b79919aecd29e02bc37e23cbc2
Matyi16 <matyas.got@umszki.hu>
2024-12-13 14:37:20 +0100

    Calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+szam1 = int(input('Első szám: '))
+szam2 = int(input('Másik szám: '))
+jel = str(input('Műveleti jel: '))
+
+
+if jel == '+' :
+    eredmeny = (szam1 + szam2)
+elif jel == '-' :
+    eredmeny = (szam1 - szam2)
+elif jel == '*' :
+    eredmeny = (szam1*szam2)
+elif jel == '/' :
+    eredmeny = (szam1/szam2)
+else:
+    print('Ismeretlen művelet')
+    
+print(f"{szam1}{jel}{szam2}={eredmeny}")
