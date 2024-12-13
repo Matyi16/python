4ffe92adc1d4d81998bbd03bbebdeefc8a03c5ab
Matyi16 <matyas.got@umszki.hu>
2024-12-13 14:53:59 +0100

    / 0

--- calculator.py
+++ calculator.py
@@ -9,9 +9,11 @@ elif jel == '-' :
     eredmeny = (szam1 - szam2)
 elif jel == '*' :
     eredmeny = (szam1*szam2)
+elif szam2 == 0 and jel == '/':
+    eredmeny = " Ez a művelet nem lehetséges!"
 elif jel == '/' :
     eredmeny = (szam1/szam2)
 else:
     print('Ismeretlen művelet')
-    
+
 print(f"{szam1}{jel}{szam2}={eredmeny}")
